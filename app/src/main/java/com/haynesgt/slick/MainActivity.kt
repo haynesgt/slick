8762fbd10b64b3d581d9b0489d5bee1877492c7a
Gavin Haynes <haynes.gt@gmail.com>
2025-01-04 15:37:18 -0800

    create sample project

---
+++ app/src/main/java/com/haynesgt/slick/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.haynesgt.slick
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.haynesgt.slick.ui.theme.SlickTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SlickTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    BakingScreen()
+                }
+            }
+        }
+    }
+}
