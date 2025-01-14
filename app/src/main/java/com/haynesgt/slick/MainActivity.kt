f76e63209eacdec0b820ed52d8a83ec326e06f12
Gavin Haynes <haynes.gt@gmail.com>
2025-01-13 20:54:54 -0800

    fullscreen mode

--- app/src/main/java/com/haynesgt/slick/MainActivity.kt
+++ app/src/main/java/com/haynesgt/slick/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.haynesgt.slick
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.LinearLayout
@@ -15,6 +17,7 @@ import java.io.FileNotFoundException
 class MainActivity : AppCompatActivity() {
     private lateinit var whiteboardViewModel: WhiteboardViewModel
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -114,11 +117,31 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                )
+
+        /*
+        val insetsController = window.insetsController
+        if (isInImmersiveMode) {
+            insetsController?.show(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+            isInImmersiveMode = false
+        } else {
+            insetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+            isInImmersiveMode = true
+        }
+         */
+
         val buttonLayout = LinearLayout(this).apply {
             orientation = LinearLayout.HORIZONTAL
             addView(previousPageButton, 0)
             addView(nextPageButton, 1)
-            // addView(clearButton, 0)
+            //addView(clearButton, 2)
         }
 
         // Add to a layout
